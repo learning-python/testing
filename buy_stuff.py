c634e8c6991af3f4012e883db249c806d7447fec
eloy ortega <info@artbyeloi.com>
2018-11-19 18:59:58 -0500

    Simplified integration testing

--- buy_stuff.py
+++ buy_stuff.py
@@ -1,3 +1,4 @@
+import calc
 class Pocket:
 
     def __init__(self, settings):
@@ -6,10 +7,11 @@ class Pocket:
         self.price = settings['price']
 
     def buy_apples(self, num_apples):
-        if self.money - self.price * num_apples >= 0:
+        needed = calc.multiply(self.price, num_apples)
+        would_be_left = calc.subtract(self.money, needed)
+
+        if would_be_left >= 0:
             self.money -= self.price * num_apples
             self.apples += num_apples
-            print("Apples: " + str(self.apples))
-            print("Money left:" + str(self.money))
         else:
             raise Exception("Not enough money to buy " + str(num_apples) + " apples")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import calc, buy_stuff
+import unittest
 
 settings = {
   'money': 10,
@@ -8,4 +9,14 @@ settings = {
 
 my_pocket = buy_stuff.Pocket(settings)
 
-my_pocket.buy_apples(3)
+class TestCalc(unittest.TestCase):
+  def test_three(self):
+    my_pocket.buy_apples(3)
+    self.assertEqual(my_pocket.apples, 3)
+    self.assertEqual(my_pocket.money, 4)
+
+  def test_three_more(self):
+    self.assertRaises(Exception, my_pocket.buy_apples, 3)
+
+if __name__ == '__main__':
+    unittest.main()
